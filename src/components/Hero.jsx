50d1e3fe9c1329def4d8fb4a475ed0847ec4cf01
SudaraWishvajith <it21221378@my.sliit.lk>
2024-01-20 12:57:05 +0530

    add animations for hero seciton

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,36 @@
 import '../styles/Hero.css';
+import { motion } from "framer-motion"
 
 export const Hero = () => {
     return (
         <div className="container">
-            <div className="topic">
+            <motion.div 
+            initial={{ opacity: 0, y: 5 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.2 }}
+            className="topic">
                 <p>Dualshock 4</p>
-            </div>
-            <div className="description">
+            </motion.div>
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="description">
                 <p>
                     Amet minim molit non deserunt uliamco est sit
                     aliqua dolor do amet sint Velt officia consequat
                     duis enim velit molit. Exercitation veniam
                     consequat sunt nostrud amet. Dolor do amet sint velt.
                 </p>
-            </div>
-            <div className="Buttons">
+            </motion.div>
+            <motion.div 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.75 }}
+            className="Buttons">
                 <div className="Buy-btn">Buy</div>
                 <div className="More-btn">More</div>
-            </div>
+            </motion.div>
         </div>
     );
 }
